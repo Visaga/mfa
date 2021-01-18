df6141a0093135c05590583e693116ea9d5f2882
AleksandrRiabov <zoom-05@mail.ru>
2021-01-18 23:25:56 +0000

    GetAndRender.js added old fetch js file deleted

---
+++ public/javaScripts/getAndRender.js
@@ -0,0 +1,93 @@
+document.addEventListener("DOMContentLoaded", () => {
+	
+const rightBar = document.querySelector("#rightBar");
+const lazyLoadingBlock = document.querySelector("#lazyLoadingBlock");
+
+	
+const category = document.querySelector("#category").textContent;
+	
+
+
+	//Render some blocks to Right Bar
+	
+	getBlogs(0,5,category)
+	.then(data => renderBlogs(rightBar, data))
+	.catch(err => console.log("Problem to load right bar content.. : " + err));
+	
+
+	
+	
+	// RENDER ON SCROLL  TO BOTTOM BLOCK 
+	
+	//------settings-----
+	const blogsPerRequest = 10;
+	const blogSize = ["col-sm-6", "col-md-4", "col-lg-3"];
+	let inProgress = false;
+	let startFrom = 0;
+	//-------------------
+	
+	
+	window.addEventListener("scroll", () => {
+		if ( (document.body.offsetHeight - window.pageYOffset) <= 1200 && inProgress == false){
+			inProgress = true;
+			
+			getBlogs(startFrom, blogsPerRequest)
+			.then(data => {
+				if (data.length > 0){
+					data = data.reverse();
+					renderBlogs(lazyLoadingBlock, data, blogSize);
+				} 
+				return data;
+			})
+			.then((data) => {
+				if (data.length == blogsPerRequest){
+					startFrom += data.length;
+					inProgress = false;
+				} else {console.log("No more data available for the request")}
+			})
+			.catch(err => console.log("SOMETHING WHENT WRONG!!! TRY AGAIN LATER:  " + err));
+		}
+	});
+	
+	
+	//===========FUNCTIONS============================
+	
+	//RENDER RECEIVED BLOGS
+	
+	function renderBlogs(parrent, data, blogSize = ["col-sm-12"]){
+		 
+		data.forEach(blog => {
+			const blogPreview = document.createElement("div");
+			       
+			blogSize.forEach(size => blogPreview.classList.add(size));
+			
+			blogPreview.innerHTML = `
+				<a href="/articles/${blog.urlExtention.replace(/ /g, "-") }/${ blog._id }" class="text-decoration-none text-dark">
+					 <div>
+							<div class="card mb-3">
+							 <img src="${ blog.content[0].img }" class="card-img-top" alt="${ blog.title }">
+							<div class="card-body">
+							<h5 class="card-title"> ${ blog.title }..</h5> 
+							  <time class="text-muted " datetime>${ blog.date }.</time>
+						 </div>
+
+						</div>
+					</div>
+				</a>`
+			parrent.append(blogPreview);
+		});
+	}
+	
+	
+	//SEND REQUEST
+
+	function getBlogs(startFrom = 0, quantity = 5, category = "All"){
+
+		return fetch(`/articles/get/${ startFrom }/${ category }/${ quantity }`)
+		.then(respond => respond.json())
+		.then(res => res)
+		.catch(err => console.log("Failed to fetch"))	
+	}
+	
+
+});
